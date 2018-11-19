aa65e6931d25bb01c7731b6c44048cfcf11c20ca
brandongt007 <brandon_gt007@outlook.com>
2018-11-19 12:16:00 -0600

    Hice comentarios en mi modelo para dar una breve explicacion

--- pensum/models.py
+++ pensum/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib import admin
 
+#modelo para las materias, por el momento solo nombre, pero se puede seguir agregando mas columnas
 class Materia(models.Model):
     nombre  =   models.CharField(max_length=50)
 
     def __str__(self):
         return self.nombre
 
+#modelo para los grados, tambien la columna nombre y aparte las muchas materias que puede seleccionar
 class Grado(models.Model):
     nombre    = models.CharField(max_length=60)
     materias = models.ManyToManyField(Materia, through='Seccion')
@@ -14,6 +16,7 @@ class Grado(models.Model):
     def __str__(self):
         return self.nombre
 
+#modelo seccion me sirvio para unir el muchos grados, muchas materias
 class Seccion(models.Model):
     grado = models.ForeignKey(Grado, on_delete=models.CASCADE)
     materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
